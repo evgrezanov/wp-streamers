ff0f9aa9b1178080ff592c2c0e4f8cd91d036057
Evgeniy Rezanov <evgrezanov@gmail.com>
2020-05-26 22:57:14 +0700

    filter for team finder

--- asset/team-finder-script.js
+++ asset/team-finder-script.js
@@ -1,171 +1,43 @@
-/**
- * from https://gist.github.com/uptimizt/34ce8e582e256eb2c3c3b612b23188a0
- 
-var FormAjax = function (elementSelector, ep, args = []) {
-  if (document.querySelector(elementSelector)) {
-    var form = document.querySelector(elementSelector);
-  } else {
-    return false;
-  }
-
-  var restApiEndpoint = wpApiSettings.root + ep;
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var formData = new FormData(form);
-    var submitBtnText = "";
-    var message = "";
-    var submitBtn = form.querySelector('input[type="submit"]');
-    var request = new XMLHttpRequest();
-
-    submitBtn.disabled = true;
-    submitBtnText = submitBtn.value;
-    submitBtn.value = "Updating team data...";
-
-    request.open("POST", restApiEndpoint);
-    request.setRequestHeader("X-WP-Nonce", wpApiSettings.nonce);
-    request.send(formData);
-
-    request.onload = function () {
-      var message = "";
-
-      if (request.status == 200) {
-        // analyze HTTP status of the response
-
-        var response = JSON.parse(request.response);
-
-        if (
-          args.successCallback &&
-          typeof args.successCallback === "function"
-        ) {
-          args.successCallback(response);
-          return;
-        }
-
-        if (response.success) {
-          message =
-            '<div class="alert alert-success" role="alert">' +
-            response.data.message +
-            "</div>";
-          teamUpdate.disply_response(message);
-        }
-        //console.log(response);
-        submitBtn.disabled = false;
-        submitBtn.value = submitBtnText;
-        //console.log(responseBlock);
-      } else if (request.status == 401) {
-        message =
-          '<div class="alert alert-danger" role="alert">' +
-          "You should login for edit team!</div>";
-        teamUpdate.disply_response(message);
-        submitBtn.disabled = false;
-        submitBtn.value = submitBtnText;
-      } else {
-        message =
-          '<div class="alert alert-danger" role="alert">' +
-          "Something wrong please try again!</div>";
-        teamUpdate.disply_response(message);
-        submitBtn.disabled = false;
-        submitBtn.value = submitBtnText;
-      }
-    };
-
-    request.onerror = function () {
-      submitBtn.disabled = false;
-      submitBtn.value = submitBtnText;
-    };
-  });
-};
-var config = window["endpointTeamUpdateProperties"];*/
-/*
-var teamUpdate = {
-  get_params: function () {
-    var arrayResult = {};
-    arrayResult["team-id"] = config["team-id"];
-    arrayResult["team-author"] = config["team-author"];
-    arrayResult["team-name"] = document.getElementById("team-name").value;
-    arrayResult["team-type"] = document.getElementById("team-type").value;
-    arrayResult["team-region"] = document.getElementById("team-region").value;
-    arrayResult["team-rank-requirements"] = document.getElementById(
-      "team-rank-requirements"
-    ).value;
-    arrayResult["team-age-requirement"] = document.getElementById(
-      "team-age-requirement"
-    ).value;
-    arrayResult["team-description"] = document.getElementById(
-      "team-description"
-    ).value;
-    jQuery(document).ready(function ($) {
-      arrayResult["team-positions-requered"] = $(
-        ".filter-option-inner-inner"
-      ).value;
+(function ($) {
+  // DataTable
+  $(document).ready(function ($) {
+    var table = $("#team-finder").DataTable({
+      responsive: true,
+      sDom: '<"top"i>r<"bottom"tflp><"clear">',
     });
-    return arrayResult;
-  },
-  disply_response: function (message) {
-    var responseBlock = document.querySelector("#teamUpdateResponse");
-    responseBlock.innerHTML = message;
-  },
-};
-
-document.addEventListener("DOMContentLoaded", function () {
-  FormAjax(
-    "#streamer-edit-team",
-    "streamers/v1/team/update/" + config["team-id"],
-    teamUpdate.get_params()
-  );
-});
-*/
-// Multiselect
-/*
-jQuery(document).ready(function ($) {
-  //console.log(config["position_required"]);
-  $("#team-positions-requered").selectpicker();
-  $("#team-positions-requered").selectpicker(
-    "val",
-    config["position_required"]
-  );
-  $("#team-positions-requered").on("changed.bs.select", function (
-    e,
-    clickedIndex,
-    isSelected,
-    previousValue
-  ) {
-    var options = $("#team-positions-requered option:selected");
-    var selected = [];
-    $(options).each(function () {
-      selected.push($(this).val());
+    // Team type filter
+    $("#team-type").on("change", function () {
+      selectedTeamType = $("#team-type option:selected").html();
+      //console.log($("#team-type option:selected").html());
+      table.search($("#team-type option:selected").html()).draw();
     });
-    $("#team-positions-requered-arr").val(JSON.stringify(selected));
+    // team-region filter
+    $("#team-region").on("change", function () {
+      selectedTeamRegion = $("#team-region option:selected").html();
+      //console.log($("#team-region option:selected").html());
+      table.search($("#team-region option:selected").html()).draw();
+    });
+    // team-rank filter
+    $("#team-rank").on("change", function () {
+      selectedTeamRank = $("#team-rank option:selected").html();
+      //console.log($("#team-rank option:selected").html());
+      table.search($("#team-rank option:selected").html()).draw();
+    });
+    // team-age filter
+    $("#team-age-requirement").on("change", function () {
+      selectedTeamAgeRequirement = $(
+        "#team-age-requirement option:selected"
+      ).html();
+      //console.log($("#team-age-requirement option:selected").html());
+      table.search($("#team-age-requirement option:selected").html()).draw();
+    });
+    // team-preferred-agent filter
+    $("#team-preferred-agent").on("change", function () {
+      selectedTeamPreferredAgent = $(
+        "#team-preferred-agent option:selected"
+      ).html();
+      //console.log($("#team-preferred-agent option:selected").html());
+      table.search($("#team-preferred-agent option:selected").html()).draw();
+    });
   });
-});
-*/
-// DataTable
-jQuery(document).ready(function ($) {
-  $("#team-finder").DataTable({
-    responsive: true,
-    /*searchPanes: true,
-    initComplete: function () {
-      this.api()
-        .columns()
-        .every(function () {
-          var column = this;
-          var select = $('<select><option value=""></option></select>')
-            .appendTo($(column.header()))
-            .on("change", function () {
-              var val = $.fn.dataTable.util.escapeRegex($(this).val());
-              column.search(val ? "^" + val + "$" : "", true, false).draw();
-            });
-
-          column
-            .data()
-            .unique()
-            .sort()
-            .each(function (d, j) {
-              select.append('<option value="' + d + '">' + d + "</option>");
-            });
-        });
-    },*/
-  });
-});
+})(window.jQuery);
--- templates/team-finder.php
+++ templates/team-finder.php
@@ -1,5 +1,4 @@
 <?php
-//https://agora-file-storage-prod.s3.us-west-1.amazonaws.com/workplace/attachment/7378606440264422621?response-content-disposition=inline%3B%20filename%3D%22specifications.PNG%22%3B%20filename%2A%3Dutf-8%27%27specifications.PNG&X-Amz-Security-Token=IQoJb3JpZ2luX2VjECYaCXVzLXdlc3QtMSJGMEQCIGSxyVLQOS%2FiA9cGmymwqFUivOczdDN8CPLVhOzS8AFGAiASaCfDOKhmPWGxPWmisnowAb1h5KHv7wKDBET27fwiFSqQBAh%2BEAAaDDczOTkzOTE3MzgxOSIMwqIgDj%2BNtU1%2BUyqkKu0DcJiiYWOEwDypC%2Bbs2QZ9zj1JwtDOVdbvRfFpWGJ8buoUtPQ4XSFQ%2BTosXY37eb%2Bl%2BISRP1Zm8YrFbr5Ma1YhXwphk0l1rOlKpbYZE7jWEiJnTbATqS%2BuUV1CF1ToLo7Xby3kpv%2F31K%2FzaUHpC6jcz3EzXnyzudgmjxzfgxep6%2BjdjnAQN6NCG2nvTjRF9kXuY6fnqkoMDMS%2FBZLnqDFr83DjXdytORAACIeBOkFHhKCQzDVbRXKDLDsnTlKFkjz50TbALbCWXWs%2BBNmdIstSJcMx449ucquImY%2BrzDZPo5kWWg3EpcV7g9XPdZ8jmaIX%2B%2BN00RraqyLA4J4pSMN1ufLI2F3clBLoMZlbgyFeIMxKJaoewNC3YiVbeDyE0OTq41pNNh0XL8ZOBKGKHaGDm5tQ7eOoCDQlDpKT%2BX%2BPPrqyxgUXoLHGrxqkzrpIyOiA%2FWC8tMj%2Fg363zfzHTJqakYrAMPUsC8Ut%2FpVwNqKETtjbtPDnAQnn2rNP64HyUJ2kAB6mh6ivP%2B2q1JywMh7lyC660qkIcEObIHFJxtGuxD9NXzvlgOY5oyUcDky51HK%2FXpZfm3VpbLwHOXhgdchfYcEfb8Ho0Xo1WibFbWWT3cZ%2FI9Bcjalb8CZbf5iQk%2F0g3hNChUNoWAF4UvHvBTD90av2BTrwAdI%2FnNqkCDq%2FJaKKKTy9CbcKYgPE5DVS5H4XR0uuoKIfDBmQvBT%2BVQJl%2BvysilIM3Fofiu1nu6KLHZtt4sGISYraeqjO4fc%2FgXtED7uor8mUEEd3L39D355SfYyfGoAOALklLqa8mc4JB2f9GR4zL3T5OJALAxUc3BglSMGQG30p89hyJnpZ5160IIRLHkVhr4ja7esgft%2FdvHAF1%2FZbzmneUpWD%2ByscaJ5nRHH2BBMfX2P9Ug7JOAYdYAwQ%2Fcm2LxSDw3IRcYhGXxRP3r%2B%2FXbRFjbIjUts16mbzQEXIRrz4Yi3UDlDcOr5Bf0RRm2SIug%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200524T233022Z&X-Amz-SignedHeaders=host&X-Amz-Expires=599&X-Amz-Credential=ASIA2YR6PYW5V2UW4PZT%2F20200524%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Signature=3ff1c814f7f26af03fd5671c9c1f2874fcbb831fc30ece20d89036fd6d9bf535
 ?>
 <div class="row">
     <div class="col-12">
@@ -7,8 +6,9 @@
             <form>
                 <div class="form-row align-items-center">
                     <div class="col-auto">
-                        <label for="team-type" class="sr-only">Team type</label>
+                        <h8>Team type</h8>
                         <select class="form-control" id="team-type" name="team-type">
+                            <option value></option>
                             <?php 
                     foreach ($all_team_type as $type): ?>
                             <option value="<?=$type->term_id?>"><?=$type->name?></option>
@@ -17,8 +17,9 @@
                         </select>
                     </div>
                     <div class="col-auto">
-                        <label for="team-type" class="sr-only">Region</label>
-                        <select class="form-control" id="team-region" name="team-type">
+                        <h8>Region</h8>
+                        <select class="form-control" id="team-region" name="team-region">
+                            <option value></option>
                             <?php 
                     foreach ($all_region as $region): ?>
                             <option value="<?=$region->term_id?>"><?=$region->name?></option>
@@ -27,8 +28,9 @@
                         </select>
                     </div>
                     <div class="col-auto">
-                        <label for="team-type" class="sr-only">Rank</label>
-                        <select class="form-control" id="team-region" name="team-type">
+                        <h8>Rank</h8>
+                        <select class="form-control" id="team-rank" name="team-rank">
+                            <option value></option>
                             <?php 
                     foreach ($all_ranks as $rank): ?>
                             <option value="<?=$rank->term_id?>"><?=$rank->name?></option>
@@ -37,8 +39,9 @@
                         </select>
                     </div>
                     <div class="col-auto">
-                        <label for="team-age_requirement" class="sr-only">Age</label>
-                        <select class="form-control" id="team-age_requirement" name="team-age_requirement">
+                        <h8>Age</h8>
+                        <select class="form-control" id="team-age-requirement" name="team-age-requirement">
+                            <option value></option>
                             <?php 
                     foreach ($ages as $key=>$value): ?>
                             <option value="<?=$key?>"><?=$value?></option>
@@ -47,8 +50,9 @@
                         </select>
                     </div>
                     <div class="col-auto">
-                        <label for="team-preferred-agent" class="sr-only">Agents</label>
+                        <h8>Agents</h8>
                         <select class="form-control" id="team-preferred-agent" name="team-preferred-agent">
+                            <option value></option>
                             <?php 
                     foreach ($agents as $key=>$value): ?>
                             <option value="<?=$key?>"><?=$value?></option>
@@ -60,16 +64,11 @@
                         <input type="text" class="form-control" id="team-name" name="team-name" value="" required>
                     </div>
                     <div class="col-auto">
-                        <button class="btn btn-primary" type="button">
-                            <?=__('Find team', 'wp-streamer')?>
-                        </button>
                         <button class="btn btn-primary" type="button" data-toggle="collapse"
                             data-target="#collapseAddNewTeam" aria-expanded="false" aria-controls="collapseAddNewTeam">
                             <?=__('Add new team', 'wp-streamer')?>
                         </button>
                     </div>
-
-
                 </div>
             </form>
         </div>
